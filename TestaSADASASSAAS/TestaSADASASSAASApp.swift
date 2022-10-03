76f5c53dcf15be20998e2c63f24dd7e2531595f5
Sraavan Chevireddy <63583829+SraavanChevireddy@users.noreply.github.com>
2022-10-04 01:05:28 +0530

    Initial Commit

---
+++ TestaSADASASSAAS/TestaSADASASSAASApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestaSADASASSAASApp.swift
+//  TestaSADASASSAAS
+//
+//  Created by Sraavan Chevireddy on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct TestaSADASASSAASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
